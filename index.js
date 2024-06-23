0d5b175364d1e4d73b90083419ffcec6e92ef798
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-06-23 13:56:07 +0200

    added css in page as a string to can visualize

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
  const http = require('http');
 
  const homePage = require('./home');
+ const siteCSS = require('./styles/site');
 
   const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.write(homePage);
+    if (req.url == '/styles/site.css')  {
+        console.log('get css');
+        res.writeHead(200, {
+            'Content-Type': 'text/css'
+        });
+
+        res.write(siteCSS);
+   
+        
+    } else {
+        
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(homePage);
+    }
+   // res.write(siteCSS);
+    
     res.end();
   });
 
-  server.listen(5000, () => console.log('Server is listening on pot 5000...'))
+  server.listen(5000, () => console.log('Server is listening on port 5000...'));
